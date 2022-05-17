69d7cfffa7b8179e5889b5d515d77a809567c774
awnpjr <awanpijarandika@gmail.com>
2022-05-17 18:25:30 +0700

    Deploy

--- chat-app/src/components/ContactList/index.js
+++ chat-app/src/components/ContactList/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import { colors, makeStyles } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
